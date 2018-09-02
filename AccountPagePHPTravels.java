20f294ca94cbffd1021f0315790a943e854624d5
SakshiTodwal <36173272+SakshiTodwal@users.noreply.github.com>
2018-09-02 11:35:53 -0400

    Add files via upload

---
+++ AccountPagePHPTravels.java
@@ -0,0 +1,29 @@
+package com.loginPHPtravels;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AccountPagePHPTravels {
+	
+	WebDriver driver;
+	By titleText = By.xpath("//div[@id='body-section']/div/div[1]/div/div[1]/h3");
+	By Home = By.xpath("//ul[@class='nav navbar-nav']/li[@class='active']/a");
+	
+	public AccountPagePHPTravels(WebDriver driver){
+
+        this.driver = driver;
+	}
+    
+	 //Get the title of Account Page
+
+    public String getAccountTitle(){
+
+     return    driver.findElement(titleText).getText();
+
+    }
+    
+  //go to Home page
+  	public void gotoHome(){
+  		driver.findElement(Home).click();
+  	}
+}
--- LoginPHPTravels.java
+++ LoginPHPTravels.java
@@ -9,30 +9,52 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginPHPTravels {
-	public static WebDriver driver = null;
-	public static void main(String[] args) throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
-		driver = new ChromeDriver();
-		String baseurl = "https://www.phptravels.net/login";
-		String username = "user@phptravels.com";
-		String pass = "demouser";
-		driver.get(baseurl);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-		WebElement email = driver.findElement(By.name("username"));
-		WebElement password = driver.findElement(By.name("password"));
-		WebElement submitLogin = driver.findElement(By.xpath("//form[@id='loginfrm']/div[1]/div[5]/button"));
-		email.sendKeys(username);
-		password.sendKeys(pass);
-		submitLogin.click();
-		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		System.out.println(driver.getTitle());
-		if(driver.getTitle() =="My Account"){
-				System.out.println("Logged in");
-			}
-		else{
-			System.out.println("Element not found");
-		}
+	WebDriver driver;
+	By email = By.name("username");
+	By password = By.name("password");
+	By submitLogin = By.xpath("//form[@id='loginfrm']/div[1]/div[5]/button");
+	
+	public LoginPHPTravels(WebDriver driver){
+		this.driver = driver;
 	}
+	
+	//set user name in textbox 
+	public void setUserName(String strUserName){
+		driver.findElement(email).sendKeys(strUserName);
+	}
+	
+	 //Set password in password textbox
+
+    public void setPassword(String strPassword){
+
+         driver.findElement(password).sendKeys(strPassword);
+    }
+    
+    //Click on login button
+
+    public void clickLogin(){
+
+         driver.findElement(submitLogin).click();
+    }       
+    
+  
+    
+    public void loginToPHPTravels(String strUserName,String strPasword){
+
+        //Fill user name
+
+        this.setUserName(strUserName);
+
+        //Fill password
+
+        this.setPassword(strPasword);
+
+        //Click Login button
+
+        this.clickLogin();        
+
+        }
+    
+   
 }
 	
